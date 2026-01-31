236c4cf06e6bd44c3b05ffcb63e3a79431c67758
shriramthebeast <shrikulk20@gmail.com>
2026-01-31 11:47:06 +0530

    fixed

--- frontend/src/components/ui/google-gemini-effect.tsx
+++ frontend/src/components/ui/google-gemini-effect.tsx
@@ -8,11 +8,7 @@ export const GoogleGeminiEffect = ({
   description,
   className,
 }: {
-<<<<<<< HEAD
   pathLengths?: (number | MotionValue<number>)[];
-=======
-  pathLengths?: number[] | MotionValue<number>[];
->>>>>>> 121a10c465cc9a1c726b10e3553215c7cba47c59
   title?: string;
   description?: string;
   className?: string;
